633a5e31c8d191ceb9e3f6e457bde0748ad5fa5c
hirocaster <hirocast@gmail.com>
2010-11-21 23:51:53 +0900

    Added method

--- application/core/Request.php
+++ application/core/Request.php
@@ -26,8 +26,35 @@ class Request
   {
     return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? true : false;
   }
-  public function getRequest()
+  public function getRequestUri()
   {
     return $_SERVER['REQUEST_URI'];
+  }
+  public function getBaseUrl()
+  {
+    $script_name = $_SERVER['SCRIPT_NAME'];
+    $request_uri = $this->getRequestUri();
+
+    if (0 === strpos($request_uri, $script_name))
+    {
+      return $script_name;
+    } elseif ( 0 === strpos($request_uri, dirname($script_name)))
+    {
+        return rtrim(dirname($script_name), '/');
+    }
+      return '';
+  }
+  public function getPathInfo()
+  {
+    $base_url = $this->getBaseUrl();
+    $request_uri = $this->getRequestUri();
+    
+    if (false !== ($pos = strpos($request_uri, '?')))
+    { 
+      $request_uri = substr($request_uri, 0, $post);
+    }
+    
+    $path_info = (string)substr($request_uri, strlen($base_url));
+    return $path_info;
   }
 }
